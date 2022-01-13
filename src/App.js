27129b5ea7a2ac8d07aad5cd1befffdebc501299
Rodrigo Diaz <rodrigohdiaz@gmail.com>
2022-01-13 17:19:29 +1300

    updated fonts and shapes

--- src/App.js
+++ src/App.js
@@ -6,16 +6,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <section className="section">
-        <h1>P1ch1</h1>
- 
-        <p>welcome to my little hideout</p>
+      <section className="section"><br></br>
+        <h1 className="header">P1ch1</h1>
+        <p className="text">welcome to my little hideout</p>
       </section>
       <section className="section">
-        <h2>My projects</h2>
+        <h2 className="header">My projects</h2>
       </section>
       <section className="section">
-        <h2>Contact me</h2>
+        <h2 className="header">Contact me</h2>
       </section>
        
       </header>
